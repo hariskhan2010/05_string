99e2a8f5a9884f9785eaeccfee780fe7fcb6a1d8
hariskhan2010 <harismalik2010afridi@gmail.com>
2025-08-26 10:20:34 +0500

    Add files via upload

---
+++ 05_string.py
@@ -0,0 +1,129 @@
+# # # # # # # # name = "haris is a good student"
+# # # # # # # # # print(name.upper())
+
+# # # # # # # # print(name)
+# # # # # # # # print(name.lower())
+# # # # # # # # print(name.capitalize())
+
+# # # # # # # # print(capital_name)
+
+# # # # # # # name = "haris"
+# # # # # # # print(name)
+# # # # # # # print(name. capitalize())
+
+# # # # # # first_name = "haris"
+# # # # # # print(first_name)
+# # # # # # print(first_name.count("a"))
+
+# # # # # # name = "hARIS"
+# # # # # # print(name)
+# # # # # # print(name.capitalize())
+
+# # # # # #Concatination
+
+# # # # # # first_name = "haris" 
+# # # # # # last_name = "afridi"
+# # # # # # full_name = first_name.capitalize() +  " " +     last_name.capitalize()
+# # # # # # full_name = (first_name +  " " +     last_name).capitalize()
+# # # # # # full_name = first_name,  " " ,     last_name
+# # # # # name = "haris", 'Afridi'
+# # # # # full_name = "-".join(name)
+# # # # # print(full_name)
+
+# # # # # name = "haris", "afridi"
+# # # # # full_name = " ". join(name)
+# # # # # print(full_name)
+
+
+
+
+# # first_name = "harsi"
+# # last_name = "afridi"
+# # full_name = first_name.capitalize() +  " "  + last_name.capitalize()
+# # print(full_name)
+
+
+
+
+
+
+
+# # # # name = "haris", "afridi"
+# # # # full_name = "_". join(name)
+# # # # print(full_name)
+
+
+
+
+
+
+# # # # #repetition
+
+# # # # laugh = "ha"
+# # # # print(laugh * 3)
+
+# # # # #indexing
+# # # # country = "Pakistanis a great country"
+# # # # print(country[6:9])
+
+
+
+
+
+
+
+
+
+# # # country = "Pakistan is a great country"
+# # # print(country[6:12])
+
+
+
+
+
+
+# # # laugh = "ha"
+# # # print(laugh * 10)
+
+# first_name = "haris"
+# last_name = "malik"
+# full_name = first_name.capitalize()+ " " + last_name.capitalize()
+# print(full_name)
+# first_name = "haris"
+# last_name = "afridi"
+# full_name = first_name.capitalize()+ " " + last_name.capitalize()
+# print(full_name)
+
+
+
+
+
+
+# country ="Pakistan"
+# print(country[6:7])
+
+
+
+
+
+
+# laugh = "ha"
+# print(laugh*8)
+
+
+
+
+
+
+# Name = "haris afrid"
+
+
+
+full_name = "haris", "afridi"
+full_name = " ". join(full_name) 
+print(full_name)
+#  print(len(full_name))
+
+# Name = "haris","afridi"
+# full_name = " ".join(Name)
+# print(full_name)
